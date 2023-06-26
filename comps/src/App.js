aff4024302ca5c299c7b5aa3f48397c3f1348926
duncrob <duncrob@uw.edu>
2023-06-25 23:25:15 -0700

    Added implementation for button using tailwind and classnames library

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,5 +1,33 @@
+import { GoBell, GoDownload, GoDatabase } from 'react-icons/go';
+import Button from "./Button";
+
 function App() {
-  return <div>App</div>;
+  return <div>
+    <div>
+      <Button secondary rounded outline>
+        <GoBell />
+        Click me!
+      </Button>
+    </div>
+    <div>
+      <Button danger outline>
+        <GoDownload />
+        Buy now!
+      </Button>
+    </div>
+    <div>
+      <Button warning>
+        <GoDatabase />
+        See deal!
+      </Button>
+    </div>
+    <div>
+      <Button secondary outline>Hide ads!</Button>
+    </div>
+    <div>
+      <Button primary rounded>Something!</Button>
+    </div>
+  </div>;
 }
 
 export default App;
