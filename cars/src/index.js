a3a727a13d5156b3713ae4f30346c53c5987eb22
duncrob <duncrob@uw.edu>
2023-07-02 17:54:10 -0700

    Created boilerplate for cars app

---
+++ cars/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom';
+import App from './App';
+
+const el = document.getElementById('root');
+const root = createRoot(el);
+
+root.render(<App />);
