743014e2af0cee086c93923a051e3dbc6bb65b94
duncrob <duncrob@uw.edu>
2023-07-04 03:22:38 -0700

    Used thunks to implement user load, add, and remove

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,5 +1,48 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { fetchUsers, addUser } from '../store';
+import Button from './Button';
+import Skeleton from './Skeleton';
+import { useThunk } from '../hooks/useThunk';
+import UsersListItem from './UsersListItem';
+
 function UsersList() {
-  return 'Users List';
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+
+  const { data } = useSelector((state) => {
+    return state.users;
+  });
+
+  useEffect(() => {
+    doFetchUsers()
+  }, [doFetchUsers]);
+
+  const handleUserAdd = () => {
+    doCreateUser();
+  };
+
+  let content;
+  if (isLoadingUsers) {
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else {
+    content = data.map((user) => {
+      return <UsersListItem key={user.id} user={user} />
+    });
+  }
+
+  if (loadingUsersError) {
+    return <div>Error fetching data...</div>;
+  }
+
+  return <div>
+    <div className="flex flex-row justify-between m-3 items-center">
+      <h1 className="m-2 text-xl">Users</h1>
+      <Button loading={isCreatingUser} onClick={handleUserAdd}>+ Add User</Button>
+      {creatingUserError && 'Error creating user...'}
+    </div>
+    {content}
+  </div>;
 }
 
 export default UsersList;
